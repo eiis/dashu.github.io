d0a5de073ddd51c85627cafda335a7a44637f704
zhangdi <1258956799@qq.com>
2022-12-04 00:28:19 +0800

    修改构建脚本

---
+++ bin/build.sh
@@ -0,0 +1,9 @@
+pnpm -F docs build
+###
+ # @Author: zhangdi 1258956799@qq.com
+ # @Date: 2022-12-04 00:26:51
+ # @LastEditors: zhangdi 1258956799@qq.com
+ # @LastEditTime: 2022-12-04 00:27:27
+ # @FilePath: /MyBlog/bin/build.sh
+ # @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+### 
