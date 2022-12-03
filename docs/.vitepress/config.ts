cd7d31d12750492dfddca47369d9801f5601dea2
zhangdi <1258956799@qq.com>
2022-12-03 23:50:05 +0800

    修改构建脚本

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,7 +2,7 @@
  * @Author: zhangdi 1258956799@qq.com
  * @Date: 2022-12-03 22:23:50
  * @LastEditors: zhangdi 1258956799@qq.com
- * @LastEditTime: 2022-12-03 22:41:47
+ * @LastEditTime: 2022-12-03 23:49:24
  * @FilePath: /MyBlog/docs/.vitepress/config.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -11,6 +11,6 @@ import { defineConfig } from "vitepress";
 export default defineConfig({
   title: "coderdashu",
   description: "大叔的博客",
-  base: "/MyBlog/",
+  // base: "/MyBlog/",
   lastUpdated: true,
 });
