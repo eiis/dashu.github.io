b9f1248de843fe165435b5cd624d3d2e915a4f8a
zhangdi <1258956799@qq.com>
2023-04-02 19:09:01 +0800

    :sparkles: 添加commitizen

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,7 +2,7 @@
  * @Author: zhangdi 1258956799@qq.com
  * @Date: 2022-12-03 22:23:50
  * @LastEditors: zhangdi 1258956799@qq.com
- * @LastEditTime: 2023-04-02 15:25:58
+ * @LastEditTime: 2023-04-02 18:54:22
  * @FilePath: /MyBlog/docs/.vitepress/config.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
