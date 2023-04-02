363f557ef2eb6d5e4a74172e9880d01457f6fec4
zhangdi <1258956799@qq.com>
2023-04-02 14:55:12 +0800

    添加自定义域名

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,17 +2,18 @@
  * @Author: zhangdi 1258956799@qq.com
  * @Date: 2022-12-03 22:23:50
  * @LastEditors: zhangdi 1258956799@qq.com
- * @LastEditTime: 2023-03-04 22:20:54
+ * @LastEditTime: 2023-04-02 14:54:52
  * @FilePath: /MyBlog/docs/.vitepress/config.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 import { defineConfig } from "vitepress";
 
 export default defineConfig({
+  base: 'https://www.fugui.info/',
   title: "coderdashu",
   description: "大叔的博客",
   lang: 'en-US',
-  base: "/dashu.github.io/",
+  // base: "/dashu.github.io/",
   lastUpdated: true, // 页面上展示最后更新的时间
   head:[
     ['link', { rel: 'icon', href: './favicon.ico' }]
